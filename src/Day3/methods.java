8e3035aecf57f89e0955575d69ce5ede04a593b2
Student <millenkahsaygirmay@gmail.com>
2025-04-09 15:26:01 -0700

    no commit

---
+++ src/Day3/methods.java
@@ -0,0 +1,26 @@
+package Day3;
+//when we don't declare the method as void we should use datatype that we want to return
+public class methods {
+    public static void greet(){
+        System.out.println("Hello World. I am in a method/function");
+    }
+    public static int add(int a, int b){
+        return a+b;
+    }
+    public static String fullName(String firstName, String lastName){
+        return firstName + " " + lastName;
+    }
+    public static String agePhrase(int age){   //use string to return,but we give to the argument is int
+        return "Your age is: " + age;
+    }
+
+    public static void main(String[] args) {
+        greet();
+        System.out.println(add(5,6));
+        int sum = add(8,9);
+        System.out.println(sum);
+        System.out.println(fullName("Mili","Girmay"));
+        System.out.println(agePhrase(26));
+    }
+
+}
